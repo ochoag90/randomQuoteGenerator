e064bedc9266c629f68f62fadd1023a134f49139
Gabe <gochoa5190@gmail.com>
2017-12-02 11:17:01 -0500

    Initial commit

---
+++ js/quotes.js
@@ -0,0 +1,46 @@
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+quotes = [
+  {
+    quote: "The truth may be stretched thin, but it never breaks, and it always surfaces above lies, as oil floats on water.",
+    author: "Miguel de Cervantes Saavedra",
+    citation: "Don Quixote",
+    year: "1605",
+    country: "Mexico",
+    wiki: 'https://en.wikipedia.org/wiki/Don_Quixote'
+  },
+  {
+    quote:"Coming together is a beginning, staying together is progress, and working together is success.",
+    author:"Henry Ford",
+    citation:"Public Speech",
+    year: "1935",
+    country: "USA",
+    wiki: 'https://en.wikipedia.org/wiki/Ford_Model_T'
+  },
+  {
+    quote:"Don't bother just to be better than your contemporaries or predecessors. Try to be better than yourself.",
+    author:"William Faulkner",
+    citation:"The Sound and the Fury",
+    year: '1929',
+    country: "USA",
+    wiki: 'https://en.wikipedia.org/wiki/Stream_of_consciousness'
+  },
+  {
+    quote:"You have to start with the truth. The truth is the only way that we can get anywhere",
+    author:"Julian Assange",
+    citation:"The Guardian",
+    year: '2010',
+    country: "Sweden",
+    wiki: 'https://en.wikipedia.org/wiki/Julian_Assange'
+  },
+  {
+    quote:"I kinda live where I find myself",
+    author:"Bob Dylan",
+    citation:"The Rolling Stone Interview",
+    year: '1984',
+    country: "USA",
+    wiki: 'https://en.wikipedia.org/wiki/Bob_Dylan'
+  }
+]
---
+++ js/script.js
@@ -0,0 +1,61 @@
+// First found a function that will pick a random number from 0->5 zero being inclusive.
+// This means that the first index inside the object, containing the function will also be chosen.
+// Found this function on this site -> https://www.w3schools.com/js/js_random.asp
+// Checked to see if the an object is passed @ random and it works.
+
+function getRandomQuote(min, max)
+  {
+    min = Math.ceil(0); //sets the minimum value
+    max = Math.floor(4);  //sets the upper value
+    var randomNumber = Math.floor(Math.random() * (4 - 0 + 1)) + min; //includes the numbers[0,1,2,3,4]
+    return quotes[randomNumber];  //returns the quote that corresponds to that index in the object
+  }
+
+// print function similar to what we have been using in the course
+function printQuote(message)
+  {
+    var quoteToPage = getRandomQuote(); //sets quoteToPage eual to the getRandomQuote function and stores that indexed value of the object
+    randomColor(); //calls the function randomColor below, which changes the background color and prints out a new quote
+
+    //assembly of the quote and the tags, I added two extra tags.
+    //Country of origin of the author of the quote, and a cool fact about the author from wiki.
+    var message = '<p class = "quote"> ' + quoteToPage.quote + ' </p>';
+        message += '<p class = "source"> ' + quoteToPage.author;
+        message += '<span class="citation"> ' + quoteToPage.citation + '</span>';
+        message += '<span class="year"> ' + quoteToPage.year + '</span>';
+        message += '<span class="country"> ' + quoteToPage.country + '</span>';
+        message += '<span class="wiki"> ' + quoteToPage.wiki + '</span></p>';
+
+
+    var div = document.getElementById('quote-box');   // Where the message is sent to within the html file/structures the page.
+    div.innerHTML = message;
+
+    return message; // returns the message each time the print quote is called.
+  }
+
+  //This function cycles through the colors that I have chosen. It chooses a random number and populates the backgroynd color to the css page.
+  function randomColor() {
+    var colors = ['Blue', 'Black', 'Green', 'LightBlue', 'LightSteelBlue', 'Violet', 'MediumVioletRed', 'SaddleBrown', 'Chocolate'];
+    var index = Math.floor(Math.random() * colors.length);
+    var selectedColor = colors[index];
+    // set random background color & call function in the printQuote function.
+    document.body.style.backgroundColor = selectedColor; //formats the css file styles.css
+  }
+  //this function was taken from the mozilla link provided, calls the function myCallback every 10 seconds
+  var intervalID = window.setInterval(myCallback, 10000);
+
+  /*function myCallback is called, that function calls the printQuote function which in turns calls the randomColor function and populates
+  the css and message to the page.
+  */
+  function myCallback() {
+  printQuote();  // Your code here
+  }
+
+  printQuote(); //starts the page on initial load
+
+
+//This function is called when the user clicks on the button.
+
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
